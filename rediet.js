3f600505245fa811a21984184439807581c28422
tesfamikal7-ui <tesfamikal7@gmail.com>
2026-01-21 19:14:07 +0300

    Add files via upload

---
+++ rediet.js
@@ -0,0 +1,11 @@
+function login() {
+    document.getElementById("status").textContent = "Online";
+}
+
+function logout() {
+    document.getElementById("status").textContent = "Offline";
+}
+
+function changeCourse() {
+    document.getElementById("course").textContent = "Computer Science";
+}
